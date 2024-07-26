816d2999dc802f683412a4dde0d3108f478e740d
Alex-Andr-19 <andrey.aleksandrychev3@mail.ru>
2024-07-26 17:25:26 +0300

    Create MainPage component

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,8 @@ export default defineConfig({
   ...base,
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@views': fileURLToPath(new URL('./src/views', import.meta.url))
     }
   }
 })
