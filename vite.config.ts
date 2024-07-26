10abcfbd565d37b852bbcd8f86a394e9d48d8be2
Alex-Andr-19 <andrey.aleksandrychev3@mail.ru>
2024-07-26 15:55:32 +0300

    Change static path

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,9 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  publicDir: process.env.NODE_ENV === 'production'
+    ? '/tg-miniapp/'
+    : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
