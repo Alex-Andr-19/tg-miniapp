50b7c525f035bbac8113ec6bea1e48d23835904f
Alex-Andr-19 <andrey.aleksandrychev3@mail.ru>
2024-07-26 15:58:20 +0300

    *

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,17 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+console.log(process.env.NODE_ENV);
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
   ],
-  publicDir: process.env.NODE_ENV === 'production'
-    ? '/tg-miniapp/'
-    : '/',
+  publicDir: process.env.NODE_ENV === 'development'
+    ? '/'
+    : '/tg-miniapp/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
