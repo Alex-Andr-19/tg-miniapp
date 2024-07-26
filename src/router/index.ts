7c160a72ea88a945d590c38ef77cb56b7c130351
Alex-Andr-19 <andrey.aleksandrychev3@mail.ru>
2024-07-26 16:41:09 +0300

    *

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,16 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: import.meta.env.BASE_URL + '/',
       name: 'home',
       component: HomeView
     },
     {
-      path: '/about',
+      path: import.meta.env.BASE_URL + '/about',
       name: 'about',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
