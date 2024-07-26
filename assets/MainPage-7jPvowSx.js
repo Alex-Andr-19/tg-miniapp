e87ef61fbcae1fe1798bdf977dcb7825e44769ec
Alex-Andr-19 <andrey.aleksandrychev3@mail.ru>
2024-07-26 17:31:44 +0300

    Updates

---
+++ assets/MainPage-7jPvowSx.js
@@ -0,0 +1,1 @@
+import{c as r,o as a}from"./index-h0kKZ0Oy.js";const s=(e,t)=>{const c=e.__vccOpts||e;for(const[n,o]of t)c[n]=o;return c},_={};function f(e,t){return a(),r("span",null,"Here!!!")}const p=s(_,[["render",f]]);export{p as default};
